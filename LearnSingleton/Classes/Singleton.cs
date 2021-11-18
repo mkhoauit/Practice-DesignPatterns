15933cf4bcf9598e2aabc832175fbb19f8fc84ec
mkhoauit <92783675+mkhoauit@users.noreply.github.com>
2021-11-18 11:13:39 +0700

    Practice Singleton

---
+++ LearnSingleton/Classes/Singleton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnSingleton.Classes
+{
+    public sealed class Singleton
+    {
+        
+        
+
+       
+        private static Singleton _instance;
+        private List<string> _servers = new List<string>();
+        private Random _random = new Random();
+
+        protected Singleton()
+        {
+            // List of available servers
+
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+            _servers.Add("ServerIV");
+            _servers.Add("ServerV");
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+
+        public string Server
+        {
+            get
+
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r].ToString();
+            }
+        }
+
+        
+    }
+}
---
+++ LearnSingleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using LearnSingleton.Classes;
+using System;
+
+namespace LearnSingleton
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+             
+            if (s1 == s2)
+            {
+                
+                Console.WriteLine($"Singleton works, both variables contain the same instance:  ");
+                
+            }
+
+            Singleton singleton = Singleton.GetInstance();
+            Console.WriteLine("How many requests do you want?");
+            uint numberRequest = Convert.ToUInt32(Console.ReadLine());
+
+            for (int i = 1; i <= numberRequest; i++)
+            {
+                string server = singleton.Server;
+                Console.WriteLine("Request " + i + " connect to : " + server);
+            }
+
+        }
+    }
+}
